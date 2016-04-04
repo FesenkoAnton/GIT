7e8c01c3e10e4523b30bce891139bc2faf01c0d3
FesenkoAnton <antonfesenkouk@gmail.com>
2016-04-04 16:45:44 +0300

    Change setSize in Hello.java
    change setSize(300,200);

--- src/GIT/Hello.java
+++ src/GIT/Hello.java
@@ -6,10 +6,10 @@ import acm.program.GraphicsProgram;
 public class Hello extends GraphicsProgram {
 
 	public void run() {
-		this.setSize(200,100);
+		this.setSize(300,200);
 		GLabel label = new GLabel("Hello");
 		label.setFont("Times New Roman-36");
-		add(label, 50, 50);
+		add(label, 70, 70);
 
 	}
 }
